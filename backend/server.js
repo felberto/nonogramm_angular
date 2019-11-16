75a56265d629830d7b49f53a63a58157415361fc
felberto <tobias.felber97@gmail.com>
2019-11-16 10:20:52 +0100

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 
@@ -7,6 +8,7 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('connected to database'));
 
+app.use(cors());
 app.use(express.json());
 
 const gameRoute = require('./routes/game.route');
