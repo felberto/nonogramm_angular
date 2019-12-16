b969d6d503d13a002df260ac2e474d44bbc83fca
Melanie Ockenfels <melanie.ockenfels@students.ffhs.ch>
2019-12-16 11:35:24 +0100

    added tutorial to navigation bar

--- frontend/src/app/tutorial/tutorial.module.ts
+++ frontend/src/app/tutorial/tutorial.module.ts
@@ -3,13 +3,14 @@ import {TutorialComponent} from "./tutorial.component";
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
 import {NgbDropdownModule} from "@ng-bootstrap/ng-bootstrap";
+import {NavigationModule} from "../navigation/navigation.module";
 
 @NgModule({
   declarations: [
     TutorialComponent
   ],
   exports: [TutorialComponent],
-  imports: [CommonModule, RouterModule, NgbDropdownModule],
+  imports: [CommonModule, RouterModule, NgbDropdownModule, NavigationModule],
   providers: []
 })
 export class TutorialModule {
